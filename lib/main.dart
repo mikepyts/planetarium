888f6d24b13a19b43c7bffc346dfcea3fba1740b
mkpyts <mk.pyts@gamil.com>
2019-05-27 22:19:57 +0300

    Pre-finalized Welcome Screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,41 @@ import 'dart:math';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
-void main() => runApp(());
+// Main gate of the application
+void main() => runApp(WelcomeScreen());
 
-
+// Animation control class
 class StaggeredAnimation extends StatelessWidget {
 
   // Constructor along with animation initialization
-  StaggeredAnimation({Key key, this.controller}):
-
-        mouth = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: controller, curve: Interval(0.8, 1.0))),
-        flip = Tween(begin: 0.0, end: 2 * pi).animate(CurvedAnimation(parent: controller, curve: Interval(0.0, 0.4, curve: Curves.bounceIn))),
-        size = Tween(begin: 80.0, end: 120.0).animate(CurvedAnimation(parent: controller, curve: Interval(0.0, 0.4, curve: Curves.bounceIn))),
-        elevation = Tween(begin: 15.0, end: 0.0).animate(CurvedAnimation(parent: controller, curve: Interval(0.5, 0.7, curve: Curves.ease))),
-
-        mouthBordersColor = ColorTween(begin: Colors.black12, end: Colors.white30).animate(CurvedAnimation(parent: controller, curve: Interval(0.8, 1.0))),
-
-        eyeColorAUR = ColorTween(begin: Colors.blueAccent, end: Colors.white12).animate(CurvedAnimation(parent: controller, curve: Interval(0.7, 1.0))),
-        eyeColorABL = ColorTween(begin: Colors.cyanAccent, end: Colors.white12).animate(CurvedAnimation(parent: controller, curve: Interval(0.7, 1.0))),
-        eyeColorBUR = ColorTween(begin: Colors.blueAccent, end: Colors.white12).animate(CurvedAnimation(parent: controller,curve: Interval(0.0, 0.5, curve: Curves.bounceIn))),
-        eyeColorBBL = ColorTween(begin: Colors.cyanAccent, end: Colors.blueAccent).animate(CurvedAnimation(parent: controller, curve: Interval(0.0, 0.5))),
-
+  StaggeredAnimation({Key key, this.controller})
+      : mouth = Tween(begin: 0.0, end: 1.0).animate(
+            CurvedAnimation(parent: controller, curve: Interval(0.8, 1.0))),
+        flip = Tween(begin: 0.0, end: 2 * pi).animate(CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.0, 0.4, curve: Curves.bounceIn))),
+        size = Tween(begin: 80.0, end: 120.0).animate(CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.0, 0.4, curve: Curves.bounceIn))),
+        elevation = Tween(begin: 15.0, end: 0.0).animate(CurvedAnimation(
+            parent: controller, curve: Interval(0.5, 0.7, curve: Curves.ease))),
+        mouthBordersColor =
+            ColorTween(begin: Colors.black12, end: Colors.white30).animate(
+                CurvedAnimation(parent: controller, curve: Interval(0.8, 1.0))),
+        eyeColorAUR = ColorTween(begin: Colors.blueAccent, end: Colors.white12)
+            .animate(
+                CurvedAnimation(parent: controller, curve: Interval(0.7, 1.0))),
+        eyeColorABL = ColorTween(begin: Colors.cyanAccent, end: Colors.white12)
+            .animate(
+                CurvedAnimation(parent: controller, curve: Interval(0.7, 1.0))),
+        eyeColorBUR = ColorTween(begin: Colors.blueAccent, end: Colors.white12)
+            .animate(CurvedAnimation(
+                parent: controller,
+                curve: Interval(0.0, 0.5, curve: Curves.bounceIn))),
+        eyeColorBBL = ColorTween(
+                begin: Colors.cyanAccent, end: Colors.blueAccent)
+            .animate(
+                CurvedAnimation(parent: controller, curve: Interval(0.0, 0.5))),
         super(key: key);
 
   // Initialize variables
@@ -40,70 +55,60 @@ class StaggeredAnimation extends StatelessWidget {
 
   final IconData iconVisible = Icons.visibility;
 
+  // Main function to build skeleton animation
   Widget _buildAnimation(BuildContext context, Widget child) {
     return Stack(children: [
-      AnimatedBuilder(
-          animation: controller,
-          builder: (context, child) {
-            return Column(children: <Widget>[
-              Expanded(
-                child: SizedBox(),
-              ),
-              SizedBox(
-                child: Container(
-                    decoration: BoxDecoration(
-                      border:
-                      Border.all(width: 1.2, color: mouthBordersColor.value),
-                    )),
-                height: MediaQuery.of(context).size.height * mouth.value,
-              ),
-              Expanded(
-                child: SizedBox(),
-              )
-            ]);
-          }),
-      Center(
-        child: GestureDetector(
-          child: AnimatedBuilder(
-            animation: controller,
-            builder: (context, child) {
-              return Transform(
-                alignment: FractionalOffset.center,
-                transform: Matrix4.rotationY(flip.value),
-                child: Material(
-                  shape: CircleBorder(),
-                  color: Colors.blueAccent[300],
-                  shadowColor: Colors.black,
-                  elevation: elevation.value,
-                  child: ShaderMask(
-                      blendMode: BlendMode.srcIn,
-                      shaderCallback: (Rect bounds) {
-                        return ui.Gradient.linear(
-                          Offset(4.0, 24.0),
-                          Offset(24.0, 4.0),
-                          [
-                            (controller.value < 0.5)
-                                ? eyeColorBBL.value
-                                : eyeColorABL.value,
-                            (controller.value < 0.5)
-                                ? eyeColorBUR.value
-                                : eyeColorAUR.value,
-                          ],
-                        );
-                      },
-                      child: Icon(
-                        iconVisible,
-                        size: controller.value < 0.5 ? size.value : 80.0,
-                      )),
-                ),
-              );
-            },
-          ),
-          onTap: () {
-            controller.forward();
-          },
+      Column(children: <Widget>[
+        Expanded(
+          child: SizedBox(),
         ),
-      ),
+        SizedBox(
+          child: Container(
+              decoration: BoxDecoration(
+            border: Border.all(width: 1.2, color: mouthBordersColor.value),
+          )),
+          height: MediaQuery.of(context).size.height * mouth.value,
+        ),
+        Expanded(
+          child: SizedBox(),
+        )
+      ]),
+      Center(
+          child: GestureDetector(
+        child: Transform(
+          alignment: FractionalOffset.center,
+          transform: Matrix4.rotationY(flip.value),
+          child: Material(
+            shape: CircleBorder(),
+            color: Colors.blueAccent[300],
+            shadowColor: Colors.black,
+            elevation: elevation.value,
+            child: ShaderMask(
+                blendMode: BlendMode.srcIn,
+                shaderCallback: (Rect bounds) {
+                  return ui.Gradient.linear(
+                    Offset(4.0, 24.0),
+                    Offset(24.0, 4.0),
+                    [
+                      (controller.value < 0.5)
+                          ? eyeColorBBL.value
+                          : eyeColorABL.value,
+                      (controller.value < 0.5)
+                          ? eyeColorBUR.value
+                          : eyeColorAUR.value,
+                    ],
+                  );
+                },
+                child: Icon(
+                  iconVisible,
+                  size: controller.value < 0.5 ? size.value : 80.0,
+                )),
+          ),
+        ),
+        onTap: () {
+          controller.forward();
+        },
+      ))
     ]);
   }
 
@@ -116,6 +121,7 @@ class StaggeredAnimation extends StatelessWidget {
   }
 }
 
+// Welcome screen class itself
 class WelcomeScreen extends StatefulWidget {
   WelcomeScreen({Key key}) : super(key: key);
 
@@ -123,80 +129,16 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
+// State control class of the Welcome Screen
 class _WelcomeScreenState extends State<WelcomeScreen>
     with SingleTickerProviderStateMixin<WelcomeScreen> {
-
   AnimationController controller;
 
-
+  // Build skeleton of the animation
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          body: Stack(children: [
-            AnimatedBuilder(
-          animation: controller,
-          builder: (context, child) {
-            return Column(children: <Widget>[
-              Expanded(
-                child: SizedBox(),
-              ),
-              SizedBox(
-                child: Container(
-                    decoration: BoxDecoration(
-                  border:
-                      Border.all(width: 1.2, color: mouthBordersColor.value),
-                )),
-                height: MediaQuery.of(context).size.height * mouth.value,
-              ),
-              Expanded(
-                child: SizedBox(),
-              )
-            ]);
-          }),
-      Center(
-        child: GestureDetector(
-          child: AnimatedBuilder(
-            animation: controller,
-            builder: (context, child) {
-              return Transform(
-                alignment: FractionalOffset.center,
-                transform: Matrix4.rotationY(flip.value),
-                child: Material(
-                  shape: CircleBorder(),
-                  color: Colors.blueAccent[300],
-                  shadowColor: Colors.black,
-                  elevation: elevation.value,
-                  child: ShaderMask(
-                      blendMode: BlendMode.srcIn,
-                      shaderCallback: (Rect bounds) {
-                        return ui.Gradient.linear(
-                          Offset(4.0, 24.0),
-                          Offset(24.0, 4.0),
-                          [
-                            (controller.value < 0.5)
-                                ? eyeColorBBL.value
-                                : eyeColorABL.value,
-                            (controller.value < 0.5)
-                                ? eyeColorBUR.value
-                                : eyeColorAUR.value,
-                          ],
-                        );
-                      },
-                      child: Icon(
-                        iconVisible,
-                        size: controller.value < 0.5 ? size.value : 80.0,
-                      )),
-                ),
-              );
-            },
-          ),
-          onTap: () {
-            controller.forward();
-          },
-        ),
-      ),
-    ]);
+        home: Scaffold(body: StaggeredAnimation(controller: controller)));
   }
 
   // Reverse animation back to start and jump to elevation
@@ -209,7 +151,13 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(duration: Duration(seconds: 6), vsync: this)..addListener(() {if (controller.value >= 0.4 && controller.value < 0.5) {reverseAndElevateDown();}});
+    controller =
+        AnimationController(duration: Duration(seconds: 6), vsync: this)
+          ..addListener(() {
+            if (controller.value >= 0.4 && controller.value < 0.5) {
+              reverseAndElevateDown();
+            }
+          });
   }
 
   // Dispose controller to prevent resources overuse
